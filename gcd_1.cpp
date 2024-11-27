a43fae47087deb8148f6a94bfe946c4a27bc5f23
vmware001 <103329245+vmware001@users.noreply.github.com>
2024-11-27 16:40:42 +0800

    Update gcd_1.cpp

--- gcd_1.cpp
+++ gcd_1.cpp
@@ -1,18 +1,24 @@
 #include <math.h>
 #include <stdio.h>
-int gcd(int a, int b,int FactorList[]);
+int FactorList[32767];int Count=0;
+int gcd(int a, int b);
 int main() {
-	int a, b;int FactorList[32767];
+	int a, b;
 	printf("Enter a:");
 	scanf_s("%d", &a);
 	printf("Enter b:");
 	scanf_s("%d", &b);
-	int gcdvalue = gcd(a, b, FactorList);
+	int gcdvalue = gcd(a, b);
 	printf("gcd=%d\n", gcdvalue);
+	printf("lcm=%d\n", gcdvalue * a * b);
+	printf("count=%d\n", Count);
+	printf("factors are:\n");
+	for (int i = 1;i <= Count;i++) {
+		printf("%d\n", FactorList[i]);
+	}
 }
 int min(int a, int b) { return a < b ? a : b; }
-int gcd(int a, int b, int FactorList[]) {
-	int Count = 0;
+int gcd(int a, int b) {
 	for (int i = 2;i <= min(a, b);i++) {
 		if (a % i == 0 && b % i == 0) {
 			Count++;
