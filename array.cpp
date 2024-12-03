fa5ecf35d3a0976bcbcac68facdbabf79635fb34
vmware001 <103329245+vmware001@users.noreply.github.com>
2024-12-03 14:01:46 +0800

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define arrayLength 7
+int main() {
+	int array[arrayLength+1] = { 2,3,7,10,23,37,65 };
+	int a;
+	printf("Enter a:");
+	scanf_s("%d", &a);
+	for (int i = 0;i <= arrayLength - 1;i++) {
+		if (a < array[i]) {
+			for (int j = arrayLength - 1;j >= i; j--) {
+				array[j + 1] = array[j];
+			}
+			array[i] = a;
+			break;
+		}
+	}
+	if (a >= array[arrayLength - 1]) {
+		array[arrayLength] = a;
+	}
+	for (int i = 0;i <= arrayLength;i++) {
+		printf("%d, ", array[i]);
+	}
+}
