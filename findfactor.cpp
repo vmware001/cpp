0dc62a8bfe8880638e170681e72dd4472beb2b95
vmware001 <103329245+vmware001@users.noreply.github.com>
2024-12-06 13:54:53 +0800

    Update findfactor.cpp

--- findfactor.cpp
+++ findfactor.cpp
@@ -6,12 +6,12 @@ int main() {
         int findFactor(long long num, long long* factorArray);
         void initArray(long long* array, int length);
         long long factorArray[arrayLength];
-        long long num; 
+        long long num;
         initArray(factorArray, arrayLength);
         printf("Enter the number you want to find its factor: ");
-        scanf_s("%lld", &num); 
+        scanf_s("%lld", &num);
         int state = findFactor(num, factorArray);
-        if (state!= 0) {
+        if (state != 0) {
             if (num < 0) {
                 printf("The number you entered can not be negative!\n\n");
             }
@@ -24,7 +24,7 @@ int main() {
                 printf("The number you entered is a prime!\n");
             }
             for (int i = 0; i <= arrayLength - 1 && factorArray[i] != 0; i++) {
-                printf("%lld", factorArray[i]); 
+                printf("%lld", factorArray[i]);
                 if (factorArray[i + 1] != 0) {
                     printf(", ");
                 }
@@ -35,13 +35,13 @@ int main() {
     return 0;
 }
 
-int findFactor(long long num, long long* factorArray) { 
+int findFactor(long long num, long long* factorArray) {
     long long i;int j;
     if (num <= 0) {
         return -1;
     }
-    long long fixedNum = num; 
-    for (i = 2, j = 0; i <= fixedNum / 2;) {
+    long long fixedNum = num;
+    for (i = 2, j = 0; i * i <= fixedNum;) {
         if (num % i == 0) {
             factorArray[j] = i;
             j++;
@@ -51,6 +51,9 @@ int findFactor(long long num, long long* factorArray) {
             i++;
         }
     }
+    if (num != 1) {
+        factorArray[j] = num;
+    }
     return 0;
 }
 
@@ -59,3 +62,95 @@ void initArray(long long* array, int length) {
         array[i] = 0;
     }
 }
+
+/*以下的代码使用了素数筛算法，但是由于在输入一个很大的数时，程序要不断遍历小于这个数的整数以判断这个数是不是素数，导致程序
+运行时间过长，算法效率过低，故舍弃
+*/
+//#include <stdio.h>
+//#include <stdbool.h>
+//#define arrayLength 1024
+//
+//int main() {
+//    while (1) {
+//        int findFactor(long long num, long long* factorArray);
+//        void initArray(long long* array, int length);
+//        long long factorArray[arrayLength];
+//        long long num; 
+//        initArray(factorArray, arrayLength);
+//        printf("Enter the number you want to find its factor: ");
+//        scanf_s("%lld", &num); 
+//        int state = findFactor(num, factorArray);
+//        if (state!= 0) {
+//            if (num < 0) {
+//                printf("The number you entered can not be negative!\n\n");
+//            }
+//            if (num == 0) {
+//                printf("The number you entered can not be zero!\n\n");
+//            }
+//        }
+//        if (state == 0) {
+//            if (factorArray[0] == 0) {
+//                printf("The number you entered is a prime!\n");
+//            }
+//            for (int i = 0; i <= arrayLength - 1 && factorArray[i] != 0; i++) {
+//                printf("%lld", factorArray[i]); 
+//                if (factorArray[i + 1] != 0) {
+//                    printf(", ");
+//                }
+//            }
+//            printf("\n\n");
+//        }
+//    }
+//    return 0;
+//}
+//
+//int findFactor(long long num, long long* factorArray) { 
+//    long long i;int j;
+//    long long prime[arrayLength];
+//    void findPrime(long long* primeArray, long long max);
+//    findPrime(prime, num);
+//    if (num <= 0) {
+//        return -1;
+//    }
+//    long long fixedNum = num; 
+//    for (i = 0, j = 0; i * i <= fixedNum && j <= arrayLength;) {
+//        if (num % prime[i] == 0) {
+//            factorArray[j] = prime[i];
+//            j++;
+//            num = num / prime[i];
+//        }
+//        else {
+//            i++;
+//        }
+//    }
+//    return 0;
+//}
+//
+//void initArray(long long* array, int length) {
+//    for (int i = 0; i <= length - 1; i++) {
+//        array[i] = 0;
+//    }
+//}
+//
+//void findPrime(long long* primeArray, long long max) {
+//
+//    int index = 0;
+//    bool isPrime[arrayLength]; 
+//    for (long long i = 0; i <= max; i++) {
+//        isPrime[i] = true;
+//    }
+//    if (max >= 0) isPrime[0] = false;
+//    if (max >= 1) isPrime[1] = false;
+//    for (long long i = 2; i * i <= max; i++) {
+//        if (isPrime[i] == true) {
+//            for (long long j = i * i; j <= max; j=j+i) {
+//                isPrime[j] = false;
+//            }
+//        }
+//    }
+//    for (long long i = 2; i <= max && index < arrayLength; i++) {
+//        if (isPrime[i]) {
+//            primeArray[index++] = i;
+//        }
+//    }
+//}
